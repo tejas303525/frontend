d6abd346efb6541da9912a4a9b8d9fcf6e95abaf
tejas <tejas303525@gmail.com>
2023-04-25 11:41:24 +0400

    completed frontend

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://commnet.onrender.com',
+    baseUrl: 'https://commnetdb.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
